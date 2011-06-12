c40919684308a38c764c2a07d6565998f84a6c0c
Dieter Lunn <coder2000@gmail.com>
2011-06-12 10:29:04 -0500

    Changed generator because we no longer need to move the static files in rails 3.1

--- generators/event_calendar/event_calendar_generator.rb
+++ generators/event_calendar/event_calendar_generator.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/lib/insert_routes.rb")
 
 class EventCalendarGenerator < Rails::Generator::Base
   default_options :static_only => false,
-                  :use_jquery => rails31?,
+                  :use_jquery => false,
                   :use_all_day => false,
                   :use_mootools => false
 
@@ -17,14 +17,16 @@ class EventCalendarGenerator < Rails::Generator::Base
 
   def manifest
     # Calculate asset path. Needs to be cleaned up to be DRY
-    asset_path = rails31? ? "app/assets" : "public"
+    #asset_path = rails31? ? "app/assets" : "public"
 
     record do |m|
-      # static files
-      m.file "stylesheet.css", "#{asset_path}/stylesheets/event_calendar.css"
+      if !rails31?
+        # static files
+        m.file "stylesheet.css", "public/stylesheets/event_calendar.css"
 
-      script = options[:use_jquery] ? 'jq_javascript.js' : (options[:use_mootools] ? 'mt_javascript.js' : 'javascript.js')
-      m.file script, "#{asset_path}/javascripts/event_calendar.js"
+        script = options[:use_jquery] ? 'jquery.js' : (options[:use_mootools] ? 'mootools.js' : 'prototype.js')
+        m.file script, "public/javascripts/event_calendar.js"
+      end
 
       # MVC and other supporting files
       unless options[:static_only]
@@ -44,11 +46,13 @@ class EventCalendarGenerator < Rails::Generator::Base
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on("--static-only",
-      "Only generate the static files. (stylesheet, javascript, and images)") { |v| options[:static_only] = v }
-    { 'jquery' => 'jQuery', 'mootools' => 'MooTools' }.each do |k,v|
-      opt.on("--use-#{k}",
-        "Use #{v} template file when generating the javascript.") { |val| options[:"use_#{v}"] = val }    
+    if !rails31?
+      opt.on("--static-only",
+        "Only generate the static files. (stylesheet, javascript, and images)") { |v| options[:static_only] = v }
+      { 'jquery' => 'jQuery', 'mootools' => 'MooTools' }.each do |k,v|
+        opt.on("--use-#{k}",
+          "Use #{v} template file when generating the javascript.") { |val| options[:"use_#{v}"] = val }    
+      end
     end
     opt.on("--use-all-day",
       "Include an 'all_day' field on events, and display appropriately.") { |v| options[:use_all_day] = v }
